f5399f770f15a0855ae8db40f924c70a1681a161
Jhon Hernandez <jhonhernandez.campus@gmail.com>
2023-05-30 12:34:23 -0500

    feat(Ejercicio 6): :memo: Ejercicio 6
    Finalizacion del ejercicio 6  de php
    
    by: Jhon Hernandez

--- ejercicios.php
+++ ejercicios.php
@@ -14,20 +14,29 @@
 
         public function darPuntuacion($informacion){
             $equiposFaltantes = array_diff_key($this->MP, $informacion);
-            var_dump($equiposFaltantes);
 
-            foreach( $equiposFaltantes as $key => $informacion){
+            foreach( $equiposFaltantes as $key => $value){
+                switch ($informacion) {
+                    case $this->W:
+                        $informacion[$key] = 0;
+                        $this->W = $informacion;
+                        break;
 
-            }
+                    case $this->D:
+                        $informacion[$key] = 0;
+                        $this->D = $informacion;
+                        break;
 
-            switch ($informacion) {
-                case $this->W:
-                    
-                    break;
-                
-                default:
-                    # code...
-                    break;
+                    case $this->L:
+                        $informacion[$key] = 0;
+                        $this->L = $informacion;
+                        break;
+
+                    case $this->P:
+                        $informacion[$key] = 0;
+                        $this->P = $informacion;
+                        break;
+                }
             }
         }
 
@@ -41,6 +50,7 @@
 
                             ($this->W[$equipo1] ?? null) ? $this->W[$equipo1] += 1 : $this->W[$equipo1] = 1; // Le estamos sumando un punto al ganador del partido
                             ($this->L[$equipo2] ?? null) ? $this->L[$equipo2] += 1 : $this->L[$equipo2] = 1; // Le estamos sumando un punto al perdedor del partido
+                            ($this->P[$equipo1] ?? null) ? $this->P[$equipo1] += 3 : $this->P[$equipo1] = 3;
                             break;
 
                         case 'draw':
@@ -49,6 +59,8 @@
 
                             ($this->D[$equipo1] ?? null) ? $this->D[$equipo1] += 1 : $this->D[$equipo1] = 1;
                             ($this->D[$equipo2] ?? null) ? $this->D[$equipo2] += 1 : $this->D[$equipo2] = 1;
+                            ($this->P[$equipo1] ?? null) ? $this->P[$equipo1] += 1 : $this->P[$equipo1] = 1;
+                            ($this->P[$equipo2] ?? null) ? $this->P[$equipo2] += 1 : $this->P[$equipo2] = 1; 
                             break;
 
                         case 'loss':
@@ -57,16 +69,14 @@
 
                             ($this->W[$equipo1] ?? null) ? $this->W[$equipo1] += 1 : $this->W[$equipo1] = 1;
                             ($this->L[$equipo2] ?? null) ? $this->L[$equipo2] += 1 : $this->L[$equipo2] = 1;
-                            break;
-                        
-                        default:
+
+                            ($this->P[$equipo1] ?? null) ? $this->P[$equipo1] += 3 : $this->P[$equipo1] = 3;
                             break;
                     }
                 } else{
                     ($this->MP[$this->equipos[$key]] ?? null) ? $this->MP[$this->equipos[$key]] += 1 : $this->MP[$this->equipos[$key]] = 1; 
                 }
             }
-
             $this->darPuntuacion($this->W);
             $this->darPuntuacion($this->D);
             $this->darPuntuacion($this->L);
@@ -77,8 +87,13 @@
     $obj = new Tournament("Allegoric Alaskans;Blithering Badgers;win;Devastating Donkeys;Courageous Californians;draw;Devastating Donkeys;Allegoric Alaskans;win;Courageous Californians;Blithering Badgers;loss;Blithering Badgers;Devastating Donkeys;loss;Allegoric Alaskans;Courageous Californians;win");
     $obj->asignarPuntos();
 
-    echo json_encode($obj->MP, JSON_PRETTY_PRINT);
-    echo json_encode($obj->W, JSON_PRETTY_PRINT);
-    echo json_encode($obj->D, JSON_PRETTY_PRINT);
-    echo json_encode($obj->L, JSON_PRETTY_PRINT);
+    function printRow($team, $mp, $w, $d, $l, $p) {
+        $row = sprintf("%-30s | %-2s | %-2s | %-2s | %-2s | %-2s", $team, $mp, $w, $d, $l, $p);
+        echo $row . PHP_EOL;
+    }
+
+    printRow("Team", "MP", "W", "D", "L", "P");
+        foreach ($obj->MP as $team => $mp) {
+        printRow($team, $mp, $obj->W[$team], $obj->D[$team], $obj->L[$team], $obj->P[$team]);
+    }
 ?>
